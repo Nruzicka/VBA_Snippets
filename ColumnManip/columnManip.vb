c7bed2a7e6ae16e3693563cff2b694f783a1deaa
Nruzicka <nruzicka.nr@gmail.com>
2023-02-13 18:25:45 +0800

    Added dynamic way to declare a range.

--- ColumnManip/columnManip.vb
+++ ColumnManip/columnManip.vb
@@ -5,3 +5,20 @@ End Function
 Public Function FULLCOLUMN(choice As Range) As String
     FULLCOLUMN = Split(choice.Address, "$")(1) & ":" & Split(choice.Address, "$")(1)
 End Function
+
+public Function colToRange(mySheet As Worksheet, _
+	startCol As String, lastCol As String, _
+	startIndex As String, Optional refCol As String = vbNullString) As Range
+
+	Dim myRange As Range
+	Dim colHeader As String
+
+	If (refCol = vbNullString) Then
+		refCol = startCol
+	End If
+
+	colHeader = startCol & startIndex & ":" & lastCol
+	Set myRange = mySheet.range(colHeader & range(refCol & Rows.Count).End(xlUp).Row)
+
+	Set colToRange = myRange
+End Function
